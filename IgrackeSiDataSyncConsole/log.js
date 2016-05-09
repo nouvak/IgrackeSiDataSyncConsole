5dda74c4033da33172d46135c3fc8f4e7c8a2865
Marko Novak <nouvak@gmail.com>
2016-05-09 21:17:32 +0200

    Added the code for writing log files.

---
+++ IgrackeSiDataSyncConsole/log.js
@@ -0,0 +1,33 @@
+﻿var winston = require('winston');
+
+winston.emitErrs = true;
+
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.File({
+            level: 'debug',
+            filename: './all-logs.log',
+            handleExceptions: true,
+            json: true,
+            maxsize: 52428800, //50MB
+            maxFiles: 5,
+            colorize: false
+        }),
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true
+        })
+    ],
+    exitOnError: false
+});
+
+logger.info('Winston logging library initialized.');
+
+module.exports = logger;
+module.exports.stream = {
+    write: function (message, encoding) {
+        logger.info(message);
+    }
+};
